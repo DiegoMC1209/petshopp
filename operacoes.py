45a04c61991d171b9e7470251bad40399d3d9c7b
DIEGO MACHADO CELIBERTO <diego.mceliberto@senacsp.edu.br>
2022-07-06 22:24:17 -0300

    first-commit

--- operacoes.py
+++ operacoes.py
@@ -11,9 +11,10 @@ def cadastrar(cpf,nome,senha,telefone,endereco,dataDeNascimento):
         sql = "insert into cliente(cpf,nome,senha,telefone,endereco,dataDeNascimento) values('{}', '{}','{}', '{}', '{}', '{}')".format(cpf,nome,senha,telefone,endereco,dataDeNascimento)
         con.execute(sql)
         db_connection.commit()
-        return con.rowcount, "Inserido"
+        return  "Cadastrado Com Sucesso"
     except Exception as erro:
-        return erro
+        return erro 
+        
 def consultar(CPF):
     try:
         sql = "select * from cliente where CPF = '{}'".format(CPF)
@@ -23,17 +24,18 @@ def consultar(CPF):
         this.msg = "Nenhum dado Encontrado!"
         for(CPF, nome, senha, telefone, endereco, dataDeNascimento) in con:
             if int(CPF) == int(CPF):
-                this.msg = "Código: {}, Nome: {}, Senha: {}, Telefone: {}, Endereço: {}, Data de Nascimento: {}".format(CPF, nome, senha, telefone, endereco, dataDeNascimento)
+                this.msg = "CPF: {} Nome: {}, Senha: {}, Telefone: {}, Endereço: {}, Data de Nascimento: {}".format(CPF,nome, senha, telefone, endereco, dataDeNascimento)
                 return this.msg
         return this.msg
     except Exception as erro:
+        this.msg = ""
         return erro
 def atualizar(CPF, campo, novoDado):
     try:
         sql = "update cliente set {} = '{}' where CPF = '{}'".format(campo, novoDado, CPF)
         con.execute(sql)
         db_connection.commit()
-        return "".format(con.rowcount)
+        return "Atualizado com Sucesso!".format(con.rowcount)
     except Exception as erro:
         return erro
 def excluir(CPF):
@@ -41,6 +43,17 @@ def excluir(CPF):
         sql = "delete from cliente where CPF = '{}'".format(CPF)
         con.execute(sql)
         db_connection.commit()
-        return "".format(con.rowcount)
+        return "Deletado com Sucesso!".format(con.rowcount)
     except Exception as erro:
         return erro
+def Login():
+    try:
+        sql = "select senha from cliente where cpf = '{}';".format(this.cpf)
+        con.execute(sql)  # prepara o comando para ser executado
+
+        for (senha) in con:
+            print(senha[0])
+            if this.senha == senha[0]:
+                print("Login Efetuado com sucesso")
+    except Exception as erro:
+        print(erro)
